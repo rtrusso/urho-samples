4a8351ad316cf3edb98c0b28813164336799cc82
Richard Russo <richardtrusso@hotmail.com>
2020-05-05 18:16:09 -0700

    SamplyGame finally running

--- SamplyGame/Desktop/Program.cs
+++ SamplyGame/Desktop/Program.cs
@@ -1,14 +1,28 @@
-﻿using Urho;
+﻿using System;
+using System.IO;
+using Urho;
 using Urho.Desktop;
 
 namespace SamplyGame.Desktop
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main(string[] _)
 		{
-			DesktopUrhoInitializer.AssetsDirectory = @"../../Assets";
-			new SamplyGame().Run();
+			var assetsPath = Path.GetFullPath(@"../../Assets");
+			DesktopUrhoInitializer.AssetsDirectory = assetsPath;
+
+			var dataPath = Path.Combine(assetsPath, "Data");
+			var options = new ApplicationOptions(assetsFolder: dataPath)
+			{
+				Height = 1024,
+				Width = 576,
+				Orientation = ApplicationOptions.OrientationType.Portrait
+			};
+
+			var result = new SamplyGame(options).Run();
+			Console.WriteLine($"Exit with status {result}");
+			Console.ReadLine();
 		}
 	}
 }
